fab64062e137982b52e8f2f71d8e8b3d3b6d1451
kingsam91 <lambetgm@gmail.com>
2020-07-28 07:56:25 -0400

    added config file

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
