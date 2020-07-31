788251b21255659ea39f48251c5d234b484809ed
kingsam91 <lambetgm@gmail.com>
2020-07-31 08:34:13 +0300

    hash user passwords

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 import datetime
 from flask import Flask
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
@@ -11,6 +12,23 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     password_hash = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    pass_secure  = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return f'User {self.username}'
